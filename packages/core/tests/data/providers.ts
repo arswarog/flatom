6f71ce536a6d987e2c41b97a3d64d9723a210611
Evgeny <arswarog@yandex.ru>
2021-09-19 00:15:20 +0300

    fix(core): remove some test data

--- packages/core/tests/data/providers.ts
+++
@@ -1,9 +0,0 @@
-import { declareProvider } from '../../src';
-
-export const Timestamp = declareProvider('timespamp', () => {
-    return new Date().getTime();
-});
-
-export const Random = declareProvider('random', () => {
-    return Math.random().toString(16).substring(2);
-});
