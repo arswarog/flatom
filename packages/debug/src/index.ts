e3d30960fb3e32a4b2226a498b5f84f2d01710d1
Arswarog <arswarog@yandex.ru>
2022-08-01 19:56:06 +0300

    feat(debug): add redux dev tools connection

---
+++ packages/debug/src/index.ts
@@ -0,0 +1,83 @@
+import { Action, Store } from '@flatom/core';
+import { EnhancerOptions } from 'redux-devtools-extension';
+
+interface DevTools {
+    connect(options: EnhancerOptions): any;
+}
+
+function getReduxDevtoolExtensionIfExists(): DevTools {
+    return typeof window !== 'undefined' && window['__REDUX_DEVTOOLS_EXTENSION__']
+        ? window['__REDUX_DEVTOOLS_EXTENSION__']
+        : null;
+}
+
+export function connectReduxDevtools(store: Store) {
+    const devToolsExtension = getReduxDevtoolExtensionIfExists();
+
+    if (!devToolsExtension) {
+        return;
+    }
+
+    const devTools = devToolsExtension.connect({
+        serialize: true,
+        features: {
+            pause: false,
+            lock: true,
+            persist: true,
+            export: true,
+            import: 'custom',
+            jump: false,
+            skip: false,
+            reorder: false,
+            dispatch: false,
+            test: false,
+        },
+    });
+
+    devTools.init(store.getState());
+
+    store.subscribe((action: Action) => {
+        const state = store.getState();
+
+        if (action.type !== '@@JUMP_TO_STATE') {
+            devTools.send(action, state);
+        }
+    });
+
+    store.onGarbageCollected(() => {
+        devTools.send({ type: '@@GARBAGE_COLLECTION' }, store.getState());
+    });
+
+    const dispatchTypes = {
+        COMMIT: () => devTools.init(store.getState()),
+        REORDER_ACTION: dispatchNotAllowed,
+        PAUSE_RECORDING: dispatchNotAllowed,
+        RESET: dispatchNotAllowed,
+        ROLLBACK: dispatchNotAllowed,
+        JUMP_TO_ACTION: dispatchNotAllowed,
+        TOGGLE_ACTION: dispatchNotAllowed,
+    };
+
+    const eventTypes = {
+        START: () => null,
+        ACTION: onAction,
+        DISPATCH: (event) => (dispatchTypes[event.payload.type] || dispatchNotAllowed)(event),
+    };
+
+    devTools.subscribe(console.log);
+    devTools.subscribe((event: any) => eventTypes[event.type]?.(event));
+
+    function dispatchNotAllowed(event) {
+        devTools.error(`Not allow dispatch ${event.payload.type}`);
+    }
+
+    function onAction(event: any) {
+        try {
+            const action = JSON.parse(event.payload);
+
+            store.dispatch(action);
+        } catch (_) {
+            devTools.error('Failed to decode action, may be action is not valid JSON');
+        }
+    }
+}
