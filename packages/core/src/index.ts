9bee61cb56dbe32f10aed37bb10561d93fa8f819
Evgeny <arswarog@yandex.ru>
2022-08-25 00:19:26 +0300

    feat(core): add declareActions and declareEffects

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,8 @@ export * from './action.types';
 export * from './atom.types';
 export * from './common';
 export * from './declare-action';
+export * from './declare-actions';
+export * from './declare-effects';
 export * from './declare-atom';
 export * from './store';
 export * from './store.types';
